dae7c8d3226cbd15df0a58868d0a7439841c1956
sig649 <sig7alista@gmail.com>
2025-02-26 03:40:06 +0900

    Add files via upload

---
+++ SimpleMod/TraitDrinkPatch.cs
@@ -0,0 +1,93 @@
+using BepInEx;
+using HarmonyLib;
+
+using UnityEngine;
+using BepInEx.Configuration;
+//using System.IO;
+//using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using System.Collections.Generic;
+using s649PBR.Main;
+
+namespace s649PBR
+{
+    namespace TraitDrinkPatchMain
+    {
+        [HarmonyPatch]
+        internal class PatchExe{
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(TraitDrink), "OnDrink")]
+            internal static void TraitDrinkPatch(TraitDrink __instance, Chara c){
+                if(PatchMain.configDebugLogging){Debug.Log("[PBR]Drinked->" + __instance.owner.id.ToString());}
+                Thing t = null;
+                string prod = "";
+                //Thing t = ThingGen.Create("potion_empty");
+                //Debug.Log("[PBR]Akibin:" + __instance.owner.id.ToString());
+                int num;
+                if(int.TryParse(__instance.owner.id, out num))
+                {//owner.idが数字
+                    switch(num)
+                    {
+                        case 928 : prod = "potion_empty";//horumon
+                        break;
+                        case 718 : prod = "1170";//coffee -> akikan
+                        break;
+                        case >= 504 and <= 505 : prod = "1170";//-> akikan
+                        break;
+                        case >= 48 and <= 59 : prod = "726";//akibin
+                        break;
+                        case >= 501 and <= 508 : prod = "726";
+                        break;
+                        case >= 718 and <= 1134 : prod = "726";
+                        break;
+                        default : prod = "potion_empty";
+                        break;
+                    }
+                } else 
+                {
+                    switch(__instance.owner.id)
+                    {//owner.idが数字以外
+                        case "bucket" : prod = "bucket_empty";
+                        break;
+                        case "crimAle" : prod = "726";
+                        break;
+                        case "milk" or "milkcan" : prod = "726";
+                        break;
+                        default : prod = "potion_empty";
+                        break;
+                    }
+                
+                }
+                if(prod != "")
+                {   
+                    if(c.IsPC)
+                    {
+                        t = ThingGen.Create(prod);
+                        c.Pick(t);
+                        if(PatchMain.configDebugLogging){Debug.Log("[PBR]Prod->" + prod + " :by " + c.GetName(NameStyle.Simple));}
+                    } else 
+                    {
+                        if(PatchMain.configFlagNpcCreatesBottlesWhenDrinking)
+                        {
+                            t = ThingGen.Create(prod);
+                            if(prod == "potion_empty")
+                            {
+                                if(EClass.rnd(2) == 0 || (c.ChildrenWeight > c.WeightLimit))
+                                    {EClass._zone.AddCard(t, c.pos);} 
+                                else 
+                                    {c.Pick(t);}
+                            } else 
+                            {
+                                if(EClass.rnd(9) == 0 || (c.ChildrenWeight > c.WeightLimit))
+                                    {EClass._zone.AddCard(t, c.pos);} 
+                                else 
+                                    {c.Pick(t);}
+                            }
+                            if(PatchMain.configDebugLogging){Debug.Log("[PBR]Prod->" + prod + " :by " + c.GetName(NameStyle.Simple));}
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SimpleMod/TraitWellPatch.cs
@@ -0,0 +1,51 @@
+using BepInEx;
+using HarmonyLib;
+
+using UnityEngine;
+using BepInEx.Configuration;
+//using System.IO;
+//using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using System.Collections.Generic;
+using s649PBR.Main;
+
+namespace s649PBR
+{
+    namespace TraitWellPatchMain
+    {
+        [HarmonyPatch]
+        internal class PatchExe 
+        {
+            //TraitWell.OnBlend実行時にも瓶を還元する
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(TraitWell), "OnBlend")]
+            internal static void TraitWellPatch(TraitWell __instance, Thing t, Chara c)
+            {
+                if(PatchMain.configDebugLogging){Debug.Log("[PBR]Blend->" + t.id.ToString());}
+                string prod = "";
+                switch(t.id)
+                {
+                    case "snow" or "drug" or "334": break;
+                    case "water" or "water_dirty" or "potion" or "perfume" or "lovepotion" or "mercury" or "blood_angel": prod = "potion_empty";
+                    break;
+                    case "330" or "331" or "335" or "336" or "338" or "928" or "1142" or "1163" or "1165" : prod = "potion_empty";
+                    break;
+                    case "bucket" : prod = "bucket_empty";
+                    break;
+                    case "milk" : prod = "726";//akibin
+                    break;
+                    case "504" or "505" or "718" : prod = "1170";//akikan
+                    break;
+                    default : prod = "726";//akibin
+                    break;
+                }
+                if(PatchMain.configDebugLogging && prod != ""){Debug.Log("[PBR]Prod->" + prod + " :by " + c.GetName(NameStyle.Simple));}
+                if(c.IsPC && prod != "")
+                {
+                    t = ThingGen.Create(prod);
+                    c.Pick(t);
+                }
+            }
+        }
+    }
+}
